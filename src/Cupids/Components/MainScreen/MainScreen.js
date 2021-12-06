3f6f70f359f140af6a7ce1755aeafe2e9feaae76
pavel <geraci98.98@gmail.com>
2021-12-06 15:52:53 +0200

    fix

--- src/Cupids/Components/MainScreen/MainScreen.js
+++ src/Cupids/Components/MainScreen/MainScreen.js
@@ -14,7 +14,7 @@ export const MainScreen = () => {
           <div className="main-screen__right">
             <h1 className="main-screen__title simple-title">Love Cupids</h1>
             <div className="main-screen__content content">
-              <p>The Love Cupids are 5000 algorithmically generated NFTs, and are part of the Sad Bear 12-week giveaway.Some Sad Bears don’t have a Valentine this year, so we are airdropping these cupids to Sad Bear holders.
+              <p>The Love Cupids are 5000 algorithmically generated NFTs, and are part of the Sad Bear 12-week giveaway. Some Sad Bears don’t have a Valentine this year, so we are airdropping these cupids to Sad Bear holders.
                 <br></br>
                 <br></br>
                 The more Sad Bears, and the more rare your Sad Bears, the higher the chance of receiving a Love Cupid.
